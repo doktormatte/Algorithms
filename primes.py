74c857b1d59add05a7b9ba008c1d30eb0c06a672
doktormatte <doktormatte@googlemail.com>
2016-05-28 13:19:38 +0200

    Add files via upload

---
+++ primes.py
@@ -0,0 +1,22 @@
+from collections import deque
+import math
+
+
+def lol(x):
+    primes = deque([2,3])
+    numbers = deque([])
+    for i in range(4,x):
+        numbers.append(i)
+    while len(numbers) != 0:
+        s = numbers.popleft()
+        for i in primes:
+            if i <= math.sqrt(s)//1:
+                if s % i != 0:
+                    prime = True
+                else:
+                    prime = False
+                    break
+        if prime:
+            primes.append(s)
+            print(s)
+    return primes
